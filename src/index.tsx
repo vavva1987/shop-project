57798bab96d4dfac773cc17f21b947e5b07c4934
Volodymyr Kysil <vavva67@gmail.com>
2023-12-17 23:13:42 +0100

    added with/without jsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+// withaut jsx
+// const el = React.createElement('h1', { id: 'title' }, 'Hello Title')
+
+// with jsx
+const a = 10
+const el = <h1 id="title">Hello Title, {a}</h1>
+root.render(<React.StrictMode>{el}</React.StrictMode>)
