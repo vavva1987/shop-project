24d437562265ec22250a638a0c7130d5843332e8
Volodymyr Kysil <vavva67@gmail.com>
2023-12-17 23:32:17 +0100

    added components

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,42 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 // without jsx
 // const el = React.createElement('h1', { id: 'title' }, 'Hello Title')
 
-// with jsx
-const a = 10
-const el = <h1 id="title">Hello Title, {a}</h1>
-root.render(<React.StrictMode>{el}</React.StrictMode>)
+// // with jsx
+// const a = 10
+// const el = <h1 id="title">Hello Title, {a}</h1>
+
+// const el = (
+//     <div>
+//         <h1>Hello React</h1>
+//         <p>
+//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit qui
+//             officiis, blanditiis ea sit, nesciunt laudantium perferendis
+//             accusantium inventore illo consectetur amet atque fugiat ipsa
+//             necessitatibus soluta debitis eligendi eos.
+//         </p>
+//         <p>
+//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit qui
+//             officiis, blanditiis ea sit, nesciunt laudantium perferendis
+//             accusantium inventore illo consectetur amet atque fugiat ipsa
+//             necessitatibus soluta debitis eligendi eos.
+//         </p>
+//     </div>
+// )
+const App = () => {
+    return (
+        <div>
+            <h1>hello world</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                voluptate, quisquam in adipisci beatae quos facilis, voluptatum
+                possimus labore quia, nulla distinctio nesciunt voluptatibus
+                vero fugiat atque sequi quam! Tempore?
+            </p>
+        </div>
+    )
+}
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
