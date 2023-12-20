dcac083e667defc799ef805d265a5602f4c3b6ad
Volodymyr Kysil <vavva67@gmail.com>
2023-12-20 22:04:38 +0100

    final commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './Header'
+import Content from './Content'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-
-const Content = () => {
-    return (
-        <React.Fragment>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Facilis vitae in tempore laborum veniam natus maiores minus? Ut
-                facere commodi tempora quia temporibus dolorem, nobis, incidunt
-                assumenda expedita ipsam debitis.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Facilis vitae in tempore laborum veniam natus maiores minus? Ut
-                facere commodi tempora quia temporibus dolorem, nobis, incidunt
-                assumenda expedita ipsam debitis.
-            </p>
-        </React.Fragment>
-    )
-}
 
 const App = () => {
     return (
         <>
             <Header title="App.js" num={10} />
             <Header title="React.js" />
-            <Content />
+            <Content p1="Hello world 1" p2="Hello world 2" bgColor="purple" />
+            <Content p1="Hello world 3" p2="Hello world 4" bgColor="red" />
+            <Content p1="Hello world 5" p2="Hello world 6" />
         </>
     )
 }
