96beb44d7e60c14c8ae25180845bb835fdf382e0
Volodymyr Kysil <vavva67@gmail.com>
2023-12-24 13:29:27 +0100

    added productsarray

--- src/utils/productsArray.ts
+++ src/utils/productsArray.ts
@@ -1,8 +1,52 @@
-const productsArray = [
+export type Products ={
+  title: string
+  description: string
+  type: string
+  capacity: string
+  price: number
+}
+
+export const productsArray: Products[] = [
+  { title:"iPhone 15 Pro",
+  description:"This is iPhone 15 Pro",
+  type:"phone",
+  capacity:"128",
+  price:1000
+  },
+  { title:"iPhone 14 Pro",
+  description:"This is iPhone 14 Pro",
+  type:"phone",
+  capacity:"256",
+  price:1500
+  },
   { title:"iPhone 13 Pro",
   description:"This is iPhone 13 Pro",
   type:"phone",
   capacity:"64",
   price:850
+  },
+  { title:"iPhone 14 Pro max",
+  description:"This is iPhone 14 Pro max",
+  type:"phone",
+  capacity:"512",
+  price:2000
+  },
+  { title:"iPhone 15",
+  description:"This is iPhone 15 Pro",
+  type:"phone",
+  capacity:"128",
+  price:1000
+  },
+  { title:"iPhone 14",
+  description:"This is iPhone 15 Pro",
+  type:"phone",
+  capacity:"128",
+  price:950
+  },
+  { title:"iPhone 13",
+  description:"This is iPhone 15 Pro",
+  type:"phone",
+  capacity:"128",
+  price:750
   }
 ]
