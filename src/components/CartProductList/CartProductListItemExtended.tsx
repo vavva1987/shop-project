28c440ef93f264cf83e4c11118e7bbb78eeb61ef
Volodymyr Kysil <vavva67@gmail.com>
2024-01-11 10:54:27 +0100

    Upd CartExtended

--- src/components/CartProductList/CartProductListItemExtended.tsx
+++ src/components/CartProductList/CartProductListItemExtended.tsx
@@ -1,6 +1,7 @@
-import { Button, Card, CardContent, Grid } from '@mui/material'
+import { Button, Card, CardContent, Grid, TextField } from '@mui/material'
 import { Products } from 'utils/productsArray'
 import DeleteIcon from '@mui/icons-material/Delete'
+import { useState } from 'react'
 
 type Props = {
     product: Products
@@ -13,6 +14,13 @@ const CartProductListItemExtended = ({
     productCount,
     removeProductFromCart,
 }: Props) => {
+    const [count, setCount] = useState<number>(1)
+    const onIncrementClick = () => {
+        setCount((prevState) => prevState + 1)
+    }
+    const onDecrementClick = () => {
+        setCount((prevState) => prevState - 1)
+    }
     return (
         <Grid item xs={12} sm={6} md={4}>
             <Card variant="outlined">
@@ -22,7 +30,25 @@ const CartProductListItemExtended = ({
                     </div>
                     <div>{product.title}</div>
                     <p>Price for one product: {product.price}</p>
-                    <p>Count: {productCount}</p>
+                    {/* <p>Count: {productCount}</p> */}
+                    <div className="product-quantity">
+                        <Button
+                            variant="outlined"
+                            onClick={onDecrementClick}
+                            disabled={count <= 1}
+                        >
+                            -
+                        </Button>
+                        <TextField size="small" value={count} />
+                        <Button
+                            variant="outlined"
+                            onClick={onIncrementClick}
+                            disabled={count >= 10}
+                        >
+                            +
+                        </Button>
+                    </div>
+                    <div className="btns-wrap"></div>
                     <Button
                         variant="outlined"
                         onClick={() => removeProductFromCart(product.id)}
